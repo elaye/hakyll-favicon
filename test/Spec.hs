4857b7d561f7e8beea62e8714ce5b9cd97275dee
elaye <genard.elie@orange.fr>
2017-04-23 12:53:10 +0100

    Add test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Hakyll
+import Hakyll.Favicon (faviconsRules)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+
+  let config = defaultConfiguration
+                  { destinationDirectory = "test/out"
+                  , providerDirectory = "test"
+                  , storeDirectory = "test/out/_cache"
+                  , inMemoryCache = True
+                  }
+
+  hakyllWith config $ do
+    faviconsRules "favicon.svg"
+
+  
